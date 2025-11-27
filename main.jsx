bbeefc83763e9456f06c66268932305e80555f10
Rodean Moradi <rodeanmoradi@users.noreply.github.com>
2025-11-27 16:04:47 -0500

    Fixed file path bug

--- main.jsx
+++ main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// CHANGED: Importing from lowercase 'app.jsx' to match your file
 import App from './app.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
